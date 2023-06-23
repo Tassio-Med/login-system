abc735bec5f00b281ebad05822015ab2da6cacbd
Tassio-Med <tassiohfmed@gmail.como>
2023-06-23 12:29:19 -0300

    feat: useAuth adicionado as rotas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import useAuth from "../hooks/useAuth";
 
 const Private = ({ Item }) => {
-  const signed = false;
+  const signed = useAuth();
 
   return signed > 0 ? <Item /> : <SignIn/>;
 }
